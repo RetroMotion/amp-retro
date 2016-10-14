cb83380bcef2b64f7ee037886f6c96f79c08c442
Ti <tikawamoto@gmail.com>
2016-10-14 16:09:27 -0400

    Add cta to header bar

--- templates/header-bar.php
+++ templates/header-bar.php
@@ -1,11 +1,8 @@
 <header id="#top" class="amp-wp-header">
 	<div>
 		<a href="<?php echo esc_url( $this->get( 'home_url' ) ); ?>">
-			<?php $site_icon_url = $this->get( 'site_icon_url' );
-				if ( $site_icon_url ) : ?>
-				<amp-img src="<?php echo esc_url( $site_icon_url ); ?>" width="32" height="32" class="amp-wp-site-icon"></amp-img>
-			<?php endif; ?>
-			<?php echo esc_html( $this->get( 'blog_name' ) ); ?>
+				<amp-img src="/wp-content/uploads/2016/10/retrologo_32px.png" width="32" class="amp-wp-site-icon"></amp-img>
 		</a>
+		<a class="amp-wp-posted-on" href="/#contact">Contact Us!</a>
 	</div>
 </header>
