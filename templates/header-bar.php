6c2ec4f978e53587a2a6629c01a88dec18c41662
Ti <tikawamoto@gmail.com>
2016-10-17 09:23:52 -0400

    add height property to logo

--- templates/header-bar.php
+++ templates/header-bar.php
@@ -1,7 +1,7 @@
 <header id="#top" class="amp-wp-header">
 	<div>
 		<a href="<?php echo esc_url( $this->get( 'home_url' ) ); ?>">
-				<amp-img src="/wp-content/uploads/2016/10/retrologo_32px.png" width="32" class="amp-wp-site-icon"></amp-img>
+				<amp-img src="/wp-content/uploads/2016/10/retrologo_32px.png" width="32" height="27" class="amp-wp-site-icon"></amp-img>
 		</a>
 		<a class="amp-wp-posted-on" href="/#contact">Contact Us!</a>
 	</div>
